33539b2f3219dc6e428906c2ff8aeb3b896caaa5
Gwen288 <wendy.dwumfuor@ashesi.edu.gh>
2025-11-04 00:03:50 +0000

    updated validations

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.getElementById('myButton').addEventListener('click', Validate)
  
 function Validate(){
-    var password=document.getElementById("center1").value.trim;
-    var c_password=document.getElementById("c_pass").value.trim;
+    var password=document.getElementById("center1").value;
+    var c_password=document.getElementById("c_pass").value;
 
     if (password===c_password){
             Swal.fire({
@@ -11,7 +11,6 @@ function Validate(){
               icon: "success",
               timer: 2000,
               timerProgressBar: true
-   
           });
           
     }else{
