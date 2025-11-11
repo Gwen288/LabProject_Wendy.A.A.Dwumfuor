57364579f371e47e156bc5ba9dfadfa4b0a29d2f
Gwen288 <wendy.dwumfuor@ashesi.edu.gh>
2025-11-11 19:39:14 +0000

    changed

---
+++ test.php
@@ -0,0 +1,25 @@
+<?
+
+echo "<h1>Hello World<h1>"
+
+$a=5;
+$b=2;
+
+add($a,$b)
+
+/**
+ * Calculates the total sum of two numbers.
+ *
+ * This function takes two numeric values and returns their sum.
+ *
+ * @param float $num1 The first number to be added.
+ * @param float $num2 The second number to be added.
+ * @return float The sum of $num1 and $num2.
+ */
+
+function add($num,$num2){
+    $num+$num2;
+}
+
+
+?>
